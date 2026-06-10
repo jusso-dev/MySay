fc17896a02fce5e930a77294aa6c89a123eecb44
Justin Middler <justin@yumait.com.au>
2026-06-10 21:43:36 +1000

    Initial Commit

---
+++ MySay/MySayApp.swift
@@ -0,0 +1,17 @@
+//
+//  MySayApp.swift
+//  MySay
+//
+//  Created by Justin Middler on 10/6/2026.
+//
+
+import SwiftUI
+
+@main
+struct MySayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
